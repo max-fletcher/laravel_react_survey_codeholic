792c83a574611eb19b866b1ed4e6e851dcedce5d
Mahin Chowdhury <54007446+max-fletcher@users.noreply.github.com>
2023-05-23 01:02:50 +0600

    First commit. Done upto 33:00.

---
+++ frontend/src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import router from './router.jsx' // new syntax for React Router. Resembles vue router.
+import { RouterProvider } from 'react-router-dom' // new syntax for React Router. Resembles vue router.
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} /> {/* new syntax for React Router. Resembles vue router. Routes are inside router.jsx above */}
+  </React.StrictMode>,
+)
