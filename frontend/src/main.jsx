db447dd8e4a8a2b54703f87f2bb2da046ffed3ec
Mahin Chowdhury <54007446+max-fletcher@users.noreply.github.com>
2023-05-26 01:11:59 +0600

    Done upto 49:30.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import router from './router.jsx' // new syntax for React Router. Resembles vue router.
 import { RouterProvider } from 'react-router-dom' // new syntax for React Router. Resembles vue router.
--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,11 +1,40 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "./App";
+import Dashboard from "./views/Dashboard";
+import Surveys from "./views/Surveys";
+import Login from "./views/Login";
+import Signup from "./views/Signup";
+import GuestLayout from "./layouts/GuestLayout";
+import DefaultLayout from "./layouts/DefaultLayout";
 
 const router = createBrowserRouter([
    {
       path: '/',
-      element: <App/>
-   }
+      element: <DefaultLayout />,
+      children: [
+         {
+            path: '/dashboard',
+            element: <Dashboard />
+         },
+         {
+            path: '/surveys',
+            element: <Surveys />
+         },
+      ]
+   },
+   {
+      path: '/',
+      element: <GuestLayout />,
+      children: [
+         {
+            path: 'login',
+            element: <Login />
+         },
+         {
+            path: '/signup',
+            element: <Signup />
+         },
+      ]
+   },
 ])
 
 export default router
