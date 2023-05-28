73472bb1fd34f8f7f501751e2f884fc1f53efdaa
Mahin Chowdhury <54007446+max-fletcher@users.noreply.github.com>
2023-05-28 16:43:27 +0600

    Done upto 1:53:20.

--- frontend/src/views/Surveys.jsx
+++ frontend/src/views/Surveys.jsx
@@ -1,6 +1,8 @@
 import PageComponent from "../components/PageComponent"
 import { useStateContext } from "../contexts/ContextProvider"
 import SurveyListItem from "../components/SurveyListItem"
+import TButton from "../components/core/TButton"
+import { PlusCircleIcon } from "@heroicons/react/24/outline"
 
 const Surveys = () => {
 
@@ -13,7 +15,13 @@ const Surveys = () => {
 
    return (
       <>
-         <PageComponent title='Surveys'>
+         {/* PASSING JSX INTO buttons PROP */}
+         <PageComponent title='Surveys' buttons={ () => (
+            <TButton color="green" to="/surveys/create">
+               <PlusCircleIcon className="h-6 w-6 mr-2"/>
+               Create New
+            </TButton>
+         )}>
             <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3">
                {surveys.map(survey => (
                   <SurveyListItem survey={survey} key={survey.id} onDeleteClick={onDeleteClick} />
